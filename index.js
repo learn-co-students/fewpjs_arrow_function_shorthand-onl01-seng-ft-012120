ff4186d829619b356845e0e115a6a5bd80622741
Daniel Rooker <dannyrooks@gmail.com>
2020-05-12 15:37:59 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function() {
+    return 2000 / 100
+}
+
+const square = function(number) {
+    return number * number
+}
+
+const add = function(one, two) {
+    return one + two
+}
